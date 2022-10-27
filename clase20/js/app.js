91b987bc1718361081ff483172b021e879d9d4f9
Maciel Castro <mlcastro@todo1.net>
2022-10-26 21:33:35 -0300

    Add clase 20

---
+++ clase20/js/app.js
@@ -0,0 +1,62 @@
+// utilice una alerta por que me parecia una forma mas sencilla de mostrar un mensaje
+// alert('Hola Comisión')        
+
+
+// aquí coloque una alerta
+// alert('Hola Comisión')        
+
+// Esto es un comentario de una linea
+
+
+/* 
+  Esto
+  es 
+  un 
+  comentario multilinea
+  */
+
+// Declaración de variables
+
+// constante -> no puede cambiar su valor
+const mentor = 'Maciel'
+const apellido = "Castro"
+const texto = 'Lorem ipsum "dolor" sit'
+
+const pi = 3.141516
+
+const comision = 26
+const comision2 = '26'
+
+
+// puede cambiar su valor
+let tutor = 'Pamela'
+
+// tutor = ''
+
+// console.log(tutor)
+// console.log(comision)
+// console.log(comision2)
+// console.log(texto)
+// console.log(apellido)
+
+
+
+// 
+const edadMaciel = 22
+const edadManuel = 19
+
+// console.log('Suma de las edades', edadMaciel + edadMaciel)
+// console.log('Resta de las edades', edadMaciel * 2)
+
+// console.log('Resta de las edades', edadMaciel - edadManuel)
+// console.log('Resta de las edades', edadMaciel / edadManuel)
+
+// let numero = 10;
+
+let yaEsHora = true
+
+console.log( !yaEsHora )
+
+
+
+
